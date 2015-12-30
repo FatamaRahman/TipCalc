038a42bd441a560d2ebe1eae07dd26667a02e4fe
Fatama Rahman <Frahman4@fordham.edu>
2015-12-30 17:35:57 -0500

    formulas added

--- Tips/ViewController.swift
+++ Tips/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Tips
 //
-//  Created by :D on 12/30/15.
+//  Created by Fatama on 12/30/15.
 //  Copyright (c) 2015 codepath. All rights reserved.
 //
 
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+
+        var billAmount = NSString(string: billField.text!).doubleValue
+        var tip = billAmount * 0.2
+        var total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
 
 }
 
