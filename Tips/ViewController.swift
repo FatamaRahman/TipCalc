4402946ea679d3177004b661c5275771f22e3e1e
Fatama Rahman <Frahman4@fordham.edu>
2015-12-30 18:43:29 -0500

    final v.1

--- Tips/ViewController.swift
+++ Tips/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
@@ -27,9 +28,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
 
         var billAmount = NSString(string: billField.text!).doubleValue
-        var tip = billAmount * 0.2
+        var tip = billAmount * tipPercentage
         var total = billAmount + tip
         
         tipLabel.text = String(format: "$%.2f", tip)
@@ -37,5 +40,8 @@ class ViewController: UIViewController {
         
     }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
